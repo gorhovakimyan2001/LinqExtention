63044aa37284df044c0b7301bbe3e43fb5ea3f17
User <User@DESKTOP-67H0QUE>
2023-02-04 00:15:56 +0400

    Add project files.

---
+++ Extentions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtention
+{
+    internal static class Extentions
+    {
+
+        public static IEnumerable<T> SkipWhile<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+            foreach(T item in collection)
+            {
+                if(!condition(item))
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> collection1, IEnumerable<T> collection2)
+        {
+            bool flag = false;
+            foreach(T item1 in collection1)
+            {
+                foreach(T item2 in collection2)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false)
+                    yield return item1;
+                flag = false;
+            }
+
+            foreach (T item2 in collection1)
+            {
+                foreach (T item1 in collection2)
+                {
+                    if (item2.Equals(item1))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false)
+                    yield return item2;
+                flag = false;
+            }
+
+        }
+
+        public static T Single<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+            foreach (T item in collection)
+                if (condition(item))
+                    return item;
+
+            return default(T);
+        }
+
+        public static bool All<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+
+            foreach (T item in collection)
+                if (!condition(item))
+                    return false;
+
+            return true;
+        }
+
+        public static bool Any<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+
+            foreach (T item in collection)
+                if (condition(item))
+                    return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LinqExtention
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>() { 4, 45, 3, 21, 34, -2, 6 };
+            List<int> list2 = new List<int>() { 7, 455, 3, -41, 24, 5, -2 };
+
+
+            Console.WriteLine(list2.Single(x => x == 100));
+
+            Console.WriteLine(list1.All(x => x > -10));
+            Console.WriteLine(list1.Any(x => x < -10));
+
+            Console.WriteLine();
+            foreach (int x in list1.Except(list2))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
